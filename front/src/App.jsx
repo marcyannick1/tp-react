67cc495e31b5e4fc7f89c75ca1cf72ea1c21c082
rufus232 <mouakassarufus@gmail.com>
2024-07-30 12:45:34 +0200

    ajout des pages connection et register

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Accueuil from "./components/Accueuil";
+import Register from './components/Register';
+import Connection from './components/Connection';
 import AjoutCab from "./components/AjoutCab";
 import Footer from "./components/footer/Footer";
 import { Link, Route, Routes } from "react-router-dom";
@@ -14,12 +16,20 @@ function App() {
       <nav>
         <Link to={"/accueuil"}>Accueuil</Link>
         <Link to={"/ajoutcab"}>AjoutCab</Link>
+        <Link to={"/Connection"}>Connection</Link>
+        <Link to={"/Register"}>Register</Link>
+
+
       </nav>
       
       <main>
         <Routes>
           <Route path="/accueuil" element={<Accueuil />}/>
           <Route path="/ajoutcab" element={<AjoutCab />}/>
+          <Route path="/Connection" element={<Connection />}/>
+          <Route path="/Register" element={<Register />}/>
+
+
         </Routes>
       </main>
 
---
+++ front/src/components/Connection.jsx
@@ -0,0 +1,41 @@
+// src/components/Connection.jsx
+
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faUserPlus, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
+import './Connection.css';
+import chien from '../assets/chien.jpeg';
+
+
+export default function Connection() {
+  return (
+    <div className="container">
+      <div className="login">
+        <div className="login-container">
+          <h1>Log in</h1>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" /><br />
+          <input type="checkbox" /><span>Remember me</span>
+          <a href="#">Forgot password?</a>
+          <button>Log in</button>
+          <hr />
+          <p>Or Connect With</p>
+          <hr />
+          <ul className="social-icons">
+            <li><FontAwesomeIcon icon={faFacebookF} size="2x" /></li>
+            <li><FontAwesomeIcon icon={faTwitter} size="2x" /></li>
+            <li><FontAwesomeIcon icon={faGithub} size="2x" /></li>
+            <li><FontAwesomeIcon icon={faLinkedinIn} size="2x" /></li>
+          </ul>
+          <div className="clearfix"></div>
+          <span className="copyright">&copy;2024</span>
+        </div>
+      </div>
+    
+      <img src={chien} alt='Description' className='image' />
+        
+ 
+    </div>
+  );
+}
